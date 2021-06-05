1e1d860557569e6835613364f69e636c17bbd568
rameshkanna555 <rameshkanna95@gmail.com>
2021-06-06 00:58:04 +0530

    inital commit

--- src/nuke_file_browser.py
+++ src/nuke_file_browser.py
@@ -1,5 +1,6 @@
 import sys
 
+import nuke
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 from PySide2.QtGui import *
@@ -7,25 +8,35 @@ from PySide2.QtGui import *
 from ui import nuke_file_browser_ui
 
 
+
 class NukeFileBrowser(nuke_file_browser_ui.Ui_MainWindow, QMainWindow):
     def __init__(self):
         super(NukeFileBrowser, self).__init__()
         self.setupUi(self)
+        self.file_path = None
+        self.add_model()
         self.FileExplorerTreeView.setContextMenuPolicy(Qt.CustomContextMenu)
         self.FileExplorerTreeView.customContextMenuRequested.connect(self.context_menu)
-        self.add_model()
+        self.FileExplorerTreeView.setColumnWidth(0, 175)
 
     def add_model(self):
         self.model = QFileSystemModel()
         self.model.setRootPath(QDir.rootPath())
         self.FileExplorerTreeView.setModel(self.model)
         self.FileExplorerTreeView.setSortingEnabled(True)
+        self.FileExplorerTreeView.clicked.connect(self.selected_file_path)
 
     def context_menu(self):
         menu = QMenu()
-        menu.addAction("Open Comp")
-        cursor = QCursor()
+        open_comp = menu.addAction("Open Comp")
+        open_comp.triggered.connect(self.open_workfile)
         menu.exec_(QCursor.pos())
+
+    def selected_file_path(self, index):
+        self.file_path = self.model.filePath(index)
+
+    def open_workfile(self):
+        nuke.scriptOpen(self.file_path)
 
 
 if __name__ == '__main__':
