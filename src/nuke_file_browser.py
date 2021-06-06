5b0c0d91ba5eb67dde74c635a6e41bd056edebca
rameshkanna555 <rameshkanna95@gmail.com>
2021-06-06 12:26:32 +0530

    inital commit

--- src/nuke_file_browser.py
+++ src/nuke_file_browser.py
@@ -1,6 +1,6 @@
 import sys
 
-import nuke
+# import nuke
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 from PySide2.QtGui import *
@@ -8,20 +8,46 @@ from PySide2.QtGui import *
 from ui import nuke_file_browser_ui
 
 
-
 class NukeFileBrowser(nuke_file_browser_ui.Ui_MainWindow, QMainWindow):
     def __init__(self):
         super(NukeFileBrowser, self).__init__()
         self.setupUi(self)
         self.file_path = None
+        self.ext_list = []
         self.add_model()
+        self.connect_ui()
         self.FileExplorerTreeView.setContextMenuPolicy(Qt.CustomContextMenu)
         self.FileExplorerTreeView.customContextMenuRequested.connect(self.context_menu)
         self.FileExplorerTreeView.setColumnWidth(0, 175)
 
+    def connect_ui(self):
+        self.NukeFilescheckBox.clicked.connect(self.create_ext_list)
+        self.MOVFilescheckBox.clicked.connect(self.create_ext_list)
+        self.PNGfilesCheckbox.clicked.connect(self.create_ext_list)
+        self.JPGfilescheckBox.clicked.connect(self.create_ext_list)
+
+    def create_ext_list(self):
+        self.ext_list.clear()
+        nuke_files = self.NukeFilescheckBox.isChecked()
+        if nuke_files:
+            self.ext_list.append(self.NukeFilescheckBox.text())
+        mov_files = self.MOVFilescheckBox.isChecked()
+        if mov_files:
+            self.ext_list.append(self.MOVFilescheckBox.text())
+        png_files = self.PNGfilesCheckbox.isChecked()
+        if png_files:
+            self.ext_list.append(self.PNGfilesCheckbox.text())
+        jpg_files = self.JPGfilescheckBox.isChecked()
+        if jpg_files:
+            self.ext_list.append(self.JPGfilescheckBox.text())
+        print(self.ext_list)
+        self.add_model()
+
     def add_model(self):
         self.model = QFileSystemModel()
         self.model.setRootPath(QDir.rootPath())
+        self.model.setNameFilters(self.ext_list)
+        self.model.setNameFilterDisables(False)
         self.FileExplorerTreeView.setModel(self.model)
         self.FileExplorerTreeView.setSortingEnabled(True)
         self.FileExplorerTreeView.clicked.connect(self.selected_file_path)
--- ui/nuke_file_browser_ui.py
+++ ui/nuke_file_browser_ui.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'nuke_file_browser_ui.ui',
 # licensing of 'nuke_file_browser_ui.ui' applies.
 #
-# Created: Fri Jun  4 23:51:24 2021
+# Created: Sun Jun  6 12:22:00 2021
 #      by: pyside2-uic  running on PySide2 5.13.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -25,12 +25,30 @@ class Ui_MainWindow(object):
         self.gridLayout_2 = QtWidgets.QGridLayout(self.frame)
         self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
         self.gridLayout_2.setObjectName("gridLayout_2")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setContentsMargins(-1, -1, -1, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.NukeFilescheckBox = QtWidgets.QCheckBox(self.frame)
+        self.NukeFilescheckBox.setChecked(False)
+        self.NukeFilescheckBox.setTristate(False)
+        self.NukeFilescheckBox.setObjectName("NukeFilescheckBox")
+        self.horizontalLayout.addWidget(self.NukeFilescheckBox)
+        self.MOVFilescheckBox = QtWidgets.QCheckBox(self.frame)
+        self.MOVFilescheckBox.setObjectName("MOVFilescheckBox")
+        self.horizontalLayout.addWidget(self.MOVFilescheckBox)
+        self.JPGfilescheckBox = QtWidgets.QCheckBox(self.frame)
+        self.JPGfilescheckBox.setObjectName("JPGfilescheckBox")
+        self.horizontalLayout.addWidget(self.JPGfilescheckBox)
+        self.PNGfilesCheckbox = QtWidgets.QCheckBox(self.frame)
+        self.PNGfilesCheckbox.setObjectName("PNGfilesCheckbox")
+        self.horizontalLayout.addWidget(self.PNGfilesCheckbox)
+        self.gridLayout_2.addLayout(self.horizontalLayout, 0, 0, 1, 1)
         self.FileExplorerTreeView = QtWidgets.QTreeView(self.frame)
         self.FileExplorerTreeView.setEditTriggers(QtWidgets.QAbstractItemView.DoubleClicked|QtWidgets.QAbstractItemView.EditKeyPressed)
         self.FileExplorerTreeView.setDragEnabled(True)
         self.FileExplorerTreeView.setWordWrap(False)
         self.FileExplorerTreeView.setObjectName("FileExplorerTreeView")
-        self.gridLayout_2.addWidget(self.FileExplorerTreeView, 0, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.FileExplorerTreeView, 1, 0, 1, 1)
         self.gridLayout.addWidget(self.frame, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -39,4 +57,8 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "MainWindow", None, -1))
+        self.NukeFilescheckBox.setText(QtWidgets.QApplication.translate("MainWindow", "*.nk", None, -1))
+        self.MOVFilescheckBox.setText(QtWidgets.QApplication.translate("MainWindow", "*.mov", None, -1))
+        self.JPGfilescheckBox.setText(QtWidgets.QApplication.translate("MainWindow", "*.jpg", None, -1))
+        self.PNGfilesCheckbox.setText(QtWidgets.QApplication.translate("MainWindow", "*.png", None, -1))
 
